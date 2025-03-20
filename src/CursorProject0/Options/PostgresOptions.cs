7a0a9b4f707e6b7d68217ab0c66886f3362a1602
Zachary Falkner <falkzach@gmail.com>
2025-03-20 13:32:06 -0600

    add organization configuration

---
+++ src/CursorProject0/Options/PostgresOptions.cs
@@ -0,0 +1,17 @@
+namespace CursorProject0.Options;
+
+public class PostgresOptions
+{
+    public const string SectionName = "Postgres";
+    
+    public string Host { get; set; } = string.Empty;
+    public string Database { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public int Port { get; set; } = 5432;
+    
+    public string GetConnectionString()
+    {
+        return $"Host={Host};Database={Database};Username={Username};Password={Password};Port={Port}";
+    }
+} 
--- src/CursorProject0/Program.cs
+++ src/CursorProject0/Program.cs
@@ -1,9 +1,19 @@
+using CursorProject0.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure options
+builder.Services.Configure<PostgresOptions>(
+    builder.Configuration.GetSection(PostgresOptions.SectionName));
+builder.Services.Configure<RedisOptions>(
+    builder.Configuration.GetSection(RedisOptions.SectionName));
+builder.Services.Configure<NatsOptions>(
+    builder.Configuration.GetSection(NatsOptions.SectionName));
 
 var app = builder.Build();
 
